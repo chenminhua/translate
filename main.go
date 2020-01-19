3446723b2c2566aaffcf49dd1666325349fe99b6
chenminhua <chenmh@blacklake.cn>
2020-01-19 20:39:26 +0800

    translate

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 // {"sentences":[{"trans":"你好，世界","orig":"hello world","backend":1}],"src":"en","spell":{}}
-type JsonType struct {
+type TranslateResult struct {
 	Sentences []Sentence `json:"sentences"`
 }
 
@@ -18,25 +20,33 @@ type Sentence struct {
 	Backend int    `json:"backend"`
 }
 
-const GOOGLE_TRANS_URL = "https://translate.googleapis.com/translate_a/single?" +
-	"client=gtx&ie=UTF-8&dj=1&sl=en&tl=zh&dt=t&q="
+const GOOGLE_TRANS_URL = "https://translate.googleapis.com/translate_a/single?"
 
-func translate(query string) {
-	res, err := http.Get(GOOGLE_TRANS_URL + query)
+func NewQueryString(sl string, tl string, query string) string {
+	return fmt.Sprintf("client=gtx&ie=UTF-8&dj=1&dt=t&sl=%s&tl=%s&q=%s", sl, tl, url.QueryEscape(query))
+}
+
+func translate(sl string, tl string, query string) string {
+	//println(GOOGLE_TRANS_URL + NewQueryString(sl, tl, query) )
+	res, err := http.Get(GOOGLE_TRANS_URL + NewQueryString(sl, tl, query) )
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
+
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	println(string(body))
-	arr := JsonType{}
-	json.Unmarshal(body, &arr)
-	println(arr.Sentences[0].Trans)
+
+	result := TranslateResult{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result.Sentences[0].Trans
 }
 
 func main() {
-	translate("hello")
+	translate("en", "zh", "Building Graphical Applications with Wasmer and WASI")
 }
